84e1f676d0035b3f7650d0386c9e229e02bbb140
Kevin Harrington <mad.hephaestus@gmail.com>
2020-05-09 13:33:47 -0400

    adding a unit test

---
+++ test/src/test/SerialTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SerialTest {
+
+	@Test
+	public void test() throws Exception {
+		NRJavaSerialTest.main(new String[0]);
+		//ReadTest.main(new String[0]);
+	}
+
+}
